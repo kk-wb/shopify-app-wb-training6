44a0c27d2169be2a16622ef56e69b71933bdb1b4
katsutoshi koseki <kk@dummy.ne.jp>
2024-04-26 20:02:06 +0900

    コメントだけ追加

--- app/routes/app.multipass.jsx
+++ app/routes/app.multipass.jsx
@@ -12,9 +12,29 @@ import {
   Form, FormLayout, TextField,
 } from "@shopify/polaris";
 
+// TODO:
+// const Multipassify = require('multipassify');
+
 export const action = async ({ request }) => {
   const form = await request.formData();
   const multiPassSecret = form.get('multiPassSecret');
+
+  // TODO: 
+  // Construct the Multipassify encoder
+  // var multipassify = new Multipassify("SHOPIFY MULTIPASS SECRET");
+
+  // Create your customer data hash
+  // var customerData = { email: 'test@example.com', remote_ip:'USERS IP ADDRESS', return_to:"http://some.url"};
+
+  // Encode a Multipass token
+  // var token = multipassify.encode(customerData);
+  
+  // Generate a Shopify multipass URL to your shop
+  // var url = multipassify.generateUrl(customerData, "yourstorename.myshopify.com");
+
+  // Generates a URL like:  https://yourstorename.myshopify.com/account/login/multipass/<MULTIPASS-TOKEN>
+
+  // TODO: ここでリダイレクト？
 }
 
 export default function MultiPassPage() {
